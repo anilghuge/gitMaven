134f0ad0cdda84b3be3ea69833d47af7756b1216
anilghuge <anilghuge780@gmail.com>
2021-11-03 21:18:33 +0530

    Stash push

--- GitExample/src/com/example/Test.java
+++ GitExample/src/com/example/Test.java
@@ -6,9 +6,16 @@ public class Test {
 		System.out.println("This m1 method");
 	}
 
+
 	public void m4() {
 		System.out.println("This m4 method");
 	}
+
+	public void m2() {
+		System.out.println("This m2 method");
+	}
+
+
 	public static void main(String[] args) {
 		System.out.println("Modified Method & updated Method ::abcd @@@222333");
 		System.out.println("this personla file updated && again updated");
