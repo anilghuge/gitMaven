7c5e12025d3ac7c29735c98acd857266cc9cad90
anilghuge <anilghuge780@gmail.com>
2021-11-03 21:10:44 +0530

    m1 method

--- GitExample/src/com/example/Test.java
+++ GitExample/src/com/example/Test.java
@@ -1,6 +1,10 @@
 package com.example;
 
 public class Test {
+	
+	public void m1() {
+		System.out.println("This m1 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Modified Method & updated Method ::abcd @@@222333");
