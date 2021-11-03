2f26a86b582c16549ad721627d86709d6dbd786a
anilghuge <anilghuge780@gmail.com>
2021-11-03 16:38:48 +0530

    push

--- demo/src/com/example/Test.java
+++ demo/src/com/example/Test.java
@@ -1,10 +1,22 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Test {
 
+	static int a;
+	static int b;
+	
 	public static void main(String[] args) {
 
-		System.out.println("Test");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a value:");
+		a=sc.nextInt();
+		System.out.println("Enter b value:");
+		b=sc.nextInt();
+		int c=a+b;
+		System.out.println("C :"+c);
+	
 	}
 
 }
